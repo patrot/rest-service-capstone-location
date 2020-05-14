8c7b47a68a2928f5140fcdc262f67a8c87ec5385
Peter Trotter <patrotter@hotmail.com>
2020-05-14 14:53:03 -0500

    Fix up method naming

--- src/test/java/com/capstone_loc/restservice/unit/service/LocationServiceTest.java
+++ src/test/java/com/capstone_loc/restservice/unit/service/LocationServiceTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class LocationServiceTest {
 
     @Test
-    public void GetAllLocationsTest() {
+    public void getAllLocationsTest() {
 
         // Arrange
 
